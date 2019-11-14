78dfd374c5c745bb41be60f02f4acb0e41558d9f
a132 <a_banderov@yahoo.com>
2019-11-14 22:08:18 +0200

    Finalize admin area of the plugin

--- src/view/metaboxes/event-date.php
+++ src/view/metaboxes/event-date.php
@@ -1,12 +1,12 @@
 <!-- <input type="text" id="datetimepicker1" class="form-control" name="sel_date" value="<?php echo !empty($date_value) ? $date_value : '' ?>"/> -->
 <label for="from">From</label>
-<input type="text" id="from" name="sel_from">
+<input type="text" id="from" name="sel_from" value="<?php echo !empty($date_value['sel_from']) ? $date_value['sel_from'] : '' ?>">
 <br />
 <label for="to">To</label>
-<input type="text" id="to" name="sel_to">
+<input type="text" id="to" name="sel_to" value="<?php echo !empty($date_value['sel_to']) ? $date_value['sel_to'] : '' ?>">
 <br />
 <label for="to">Time</label>
-<input type="text" id="time" name="sel_time">
+<input type="text" id="time" name="sel_time" value="<?php echo !empty($date_value['sel_time']) ? $date_value['sel_time'] : '' ?>">>
 
 <script type="text/javascript">
     jQuery(document).ready(function($) {
